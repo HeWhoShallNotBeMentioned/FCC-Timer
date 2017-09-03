3c120bbd1495b76c11741d94e00173f7a1d0774b
Chris Underwood <cunderwoodmn@gmail.com>
2017-09-02 22:38:09 -0700

    most of js is finished

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  function updateClock(){
+    var t = getTimeRemaining(endtime);
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = t.hours;
+      minutesSpan.innerHTML = t.minutes;
+      secondsSpan.innerHTML = t.seconds;
+    if(t.total<=0){
+      document.getElementById("completed").innerHTML = "Finished!";
+      clearInterval(timeinterval);
+    }
+  }
+  updateClock();
+  var timeinterval = setInterval(updateClock,1000);
+}
+
+document.getElementById("submit").onclick = function()
+  {myFunction();};
+
+function myFunction() {
+
+  var x = Number(document.getElementById("textbox").value);
+  console.log("value from x in box:  ", x);
+  console.log("type of x  ", typeof x);
+  var timeInMinutes = x;
+  var currentTime = Date.parse(new Date());
+  var deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+  initializeClock('displayTime', deadline);
+}
